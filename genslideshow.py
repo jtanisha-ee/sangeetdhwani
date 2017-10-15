1eeb72263cbabb97f4fe4a37bb26b882f837df4b
Manoj Joshi <manojjoshi@Manojs-MacBook-Pro.local>
2017-10-15 15:42:23 -0700

    added recent concert photos of Oct 9

--- genslideshow.py
+++ genslideshow.py
@@ -14,11 +14,11 @@ with open('genshow.html', 'w') as gen:
     gen.write(header + '\n')
 
     IMAGES = 'images'
-    #IMAGES = '../../Downloads/slideshow/images'
-    list = os.listdir(IMAGES)
+    #list = os.listdir(IMAGES)
+    list = [os.path.join(dp, ff) for dp, dn, filenames in os.walk(IMAGES) for ff in filenames if "." in ff]
     numFiles = len(list)
     for ii,file in enumerate(list):
-        block = '<div class="mySlides fade">\n<div class="numbertext">{}/{}</div>\n<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="{}" style="width:60%">\n</div>'.format(ii+1, numFiles, '/'.join([IMAGES, file]))
+        block = '<div class="mySlides fade">\n<div class="numbertext">{}/{}</div>\n<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="{}" style="width:60%">\n</div>'.format(ii+1, numFiles, file)
         #print block
         gen.write(block + '\n')
     
