b36558e1f9675f1515fd7dba2f3fec1d544d7a8c
Manoj Joshi <manojjoshi@Manojs-MacBook-Pro.local>
2017-10-15 20:53:20 -0700

    randomizing images

--- genslideshow.py
+++ genslideshow.py
@@ -14,7 +14,7 @@ with open('genshow.html', 'w') as gen:
     list = [os.path.join(dp, ff) for dp, dn, filenames in os.walk(IMAGES) for ff in filenames if "." in ff]
     numFiles = len(list)
     for ii,file in enumerate(list):
-        block = '<div class="mySlides fade">\n<div class="numbertext">{}/{}</div>\n<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="{}" style="width:60%">\n</div>'.format(ii+1, numFiles, file)
+        block = '<div class="mySlides fade">\n<!--<div class="numbertext">{}/{}</div>\n--><img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="{}" style="width:60%">\n</div>'.format(ii+1, numFiles, file)
         #print block
         gen.write(block + '\n')
     
